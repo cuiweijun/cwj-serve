2a1c3d26433a1f7a9a86cbc717ebcb946d9c17d6
崔为俊 <834719201@qq.com>
2020-12-15 11:18:11 +0800

    second commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## cwj-serve
+download
+npm install cwj-serve -g 
+how to use
+cwj-serve
+
+Open the browser and enter the address
--- bin/serve.js
+++ bin/serve.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 // ----native----
+const os = require("os")
 
 //-----package---
 const boxen = require("boxen")
@@ -7,9 +8,24 @@ const express = require("express")
 const app = express()
 app.use(express.static(process.cwd()));
 //---
+//获取 network ip
+const interfaces = os.networkInterfaces();
+const getNetworkAddress = () => {
+	for (const name of Object.keys(interfaces)) {
+		for (const interface of interfaces[name]) {
+			const {address, family, internal} = interface;
+			if (family === 'IPv4' && !internal) {
+				return address;
+			}
+		}
+	}
+};
+const address = getNetworkAddress()
+
 let message = `
     Serving!                                        
     - Local:            http://localhost:9000
+    - Network           http://${address}:9000
     Copied local address to clipboard!
 `
 app.listen(9000,()=>{
